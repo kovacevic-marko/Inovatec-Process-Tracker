5c9521040b6d15cab14cff94f5f77f38d495dbcc
Nikola Dukic <ndukic25@gmail.com>
2017-10-11 11:32:59 +0200

    Kreiran backend projekat

---
+++ Backend/IPTWebAPI/IPTWebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace IPTWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Backend/IPTWebAPI/IPTWebAPI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace IPTWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
