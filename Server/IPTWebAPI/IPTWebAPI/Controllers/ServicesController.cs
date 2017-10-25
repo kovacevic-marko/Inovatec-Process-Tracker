52409d73d9ab6612bbd1aee2557485cae23ad400
mblacanin <miroslavblacanin92@gmail.com>
2017-10-25 12:42:40 +0200

    Kreirani nov projekat(class library .net framework) u server solution-u, napravljen ado.net model sa konekcijom na nasu lokalnu bazu. U IPTWebAPI projektu napravljen Services kontroler koji vraca xml sa ToList metodom koji sadrzi sve podatke tabele tb_services.

---
+++ Server/IPTWebAPI/IPTWebAPI/Controllers/ServicesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IPTDataAccess;
+
+namespace IPTWebAPI.Controllers
+{
+    public class ServicesController : ApiController
+    {
+        public IEnumerable<tb_service> Get()
+        { 
+            using(InovatecDBEntities entities=new InovatecDBEntities())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.tb_service.ToList();
+            }
+        }
+    }
+}
