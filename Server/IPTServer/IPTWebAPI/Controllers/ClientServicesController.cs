43fd399c64b650da5b8d68129d4b7773ed68d31f
petarljusic <petarljusic94@gmail.com>
2017-11-02 15:13:35 +0100

    Dodani kontroleri u IPTWebAPI

---
+++ Server/IPTServer/IPTWebAPI/Controllers/ClientServicesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using IPTDataAccess;
+namespace IPTWebAPI.Controllers
+{
+    public class ClientServiceController : ApiController
+    {
+        public IEnumerable<GetServicesForClientID_Result> Get(int ClientId)
+        {
+            using (IPTDBEntities entities = new IPTDBEntities())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                SqlParameter param1 = new SqlParameter("@ClientID", ClientId);
+                return entities.Database.SqlQuery<GetServicesForClientID_Result>("GetServicesForClientID @ClientID", param1).ToList();
+            }
+        }
+
+    }
+}
