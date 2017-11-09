9dce82c7bb405a59d313b669f6ee1b2dae04940a
mblacanin <miroslavblacanin92@gmail.com>
2017-11-09 09:50:12 +0100

    implementirana lista prikaz na pocetnoj strani, delimicno stilizovana

--- KlijentXF/IPTXamarinForms/IPTXamarinForms/IPTXamarinForms/Views/MainPage.xaml.cs
+++ KlijentXF/IPTXamarinForms/IPTXamarinForms/IPTXamarinForms/Views/MainPage.xaml.cs
@@ -24,30 +24,39 @@ namespace IPTXamarinForms
             string url = "http://172.24.2.51:5000/api/clients";
             string jsonString = await JsonFunctions.GetJson(url);
             List<ClientModel> clients = JsonConvert.DeserializeObject<List<ClientModel>>(jsonString);
-            
+
             var stackLayoutVertical = new StackLayout()
             {
                 Orientation = StackOrientation.Vertical,
             };
 
+            List<string> clientNames = new List<string>();
+
             foreach (var client in clients)
             {
-                Button btn = new Button
-                {
-                    Text = client.ClientID + " " + client.ClientName,
-                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                    HorizontalOptions = LayoutOptions.Center,
-                };
-                btn.Clicked += (sender, args) => { Navigation.PushAsync(new Client(client.ClientID, client.ClientName)); };
 
-                stackLayoutVertical.Children.Add(btn);
+                //btn.Clicked += (sender, args) => { Navigation.PushAsync(new Client(client.ClientID, client.ClientName)); };
+
+                //stackLayoutVertical.Children.Add(btn);
             }
-            // Accomodate iPhone status bar.
-            //this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            listClient.ItemsSource = clients;
+            // listClient.ItemTapped += (sender, args) => { Navigation.PushAsync(new Client(listClient.SelectedItem., client.ClientName)); };
+            stackLayoutVertical.Children.Add(listClient);
 
             // Build the page.
             this.Content = new ScrollView { Content = stackLayoutVertical };
         }
 
+        private void listClient_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //DisplayAlert("Item Selected", ((ListView)sender).SelectedItem.GetType().ToString(), "Ok");
+            ClientModel m = new ClientModel();
+            m = (ClientModel)((ListView)sender).SelectedItem;
+            //DisplayAlert("Item Selected", m.ClientName + " id= " + m.ClientID, "Ok");
+            Navigation.PushAsync(new Client(m.ClientID,m.ClientName));
+        }
+
+      
     }
 }
