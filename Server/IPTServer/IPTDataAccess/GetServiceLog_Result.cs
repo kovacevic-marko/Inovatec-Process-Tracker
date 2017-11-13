55943e1fa53573b0dc465236e861bd09d2144306
kovacevic-marko <markokovacevic97@gmail.com>
2017-11-13 09:12:02 +0100

    Komitovan je WebAPI za vracanje tabele Log informacija  koji je bio izmenjen  11-9

--- Server/IPTServer/IPTDataAccess/GetServiceLog_Result.cs
+++ Server/IPTServer/IPTDataAccess/GetServiceLog_Result.cs
@@ -15,7 +15,8 @@ namespace IPTDataAccess
     {
         public int LogID { get; set; }
         public int ClientServiceID { get; set; }
-        public System.DateTime LogDate { get; set; }
+        public DateTime OfflineFrom { get; set; }
+        public Nullable<DateTime> OfflineTo { get; set; }
         public Nullable<int> StatusCode { get; set; }
         public string StatusDescription { get; set; }
         public string Error { get; set; }
